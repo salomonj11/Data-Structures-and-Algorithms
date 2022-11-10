29f2cfb89e498e2112fd3b294411ecb470a6c53e
Jesus Salomon <80434233+salomonj11@users.noreply.github.com>
2022-11-09 18:02:01 -0600

    Update ReverseString.js

--- ReverseString.js
+++ ReverseString.js
@@ -11,3 +11,32 @@ order of characters
 function reverse(str) {
   return str.split('').reverse().join('');
 }
+
+
+---- Other Ways To Solve ----
+
+1) For Loop
+
+// Create an empty string called reversed
+// For each character in the provided string, take the character and add it to the start of reversed
+//return the variable reversed
+
+function reverse(str) {
+  let reversed = '';
+  
+  for (let c of str) {
+    reversed = c + reversed;
+}
+  return reversed;
+}
+
+2) Reduce
+
+// Take string, turn into array using split function, use reduce helper
+
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+
+
