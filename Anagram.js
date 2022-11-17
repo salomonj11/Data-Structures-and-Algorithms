9c44441b62f664a247f3226a8e96c7ce49901a7a
Jesus Salomon <80434233+salomonj11@users.noreply.github.com>
2022-11-16 23:20:02 -0600

    Create Anagram.js

---
+++ Anagram.js
@@ -0,0 +1,33 @@
+--- Directions
+Check to see if two provided strings are anagrams of eachother.
+One string is an anagram of another if it uses the same characters
+in the same quantity. Only consider characters, not spaces
+or punctuation.  Consider capital letters to be the same as lower case
+--- Examples
+  anagrams('rail safety', 'fairy tales') --> True
+  anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+  anagrams('Hi there', 'Bye there') --> False
+
+  
+ ---- Solution ----
+
+// Create helper function 'cleanString'
+// Use replace to get rid of special characters and spaces
+// Use toLowerCase to make all same
+// Use Split to turn str into array
+// Use Sort to sort through array
+// Use Join to turn array into str
+// Use cleanString helper function to compare stringA and stringB
+
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+  return str
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+}
