389a23f53364a462722baff09376456fb7c56f30
Jesus Salomon <80434233+salomonj11@users.noreply.github.com>
2022-11-28 21:22:57 -0600

    Update Steps.js

--- Steps.js
+++ Steps.js
@@ -1,1 +1,43 @@
+--- Directions ---
+Write a function that accepts a positive number N.
+The function should console log a step shape
+with N levels using the # character.  Make sure the
+step has spaces on the right hand side!
+--- Examples
+  steps(2)
+      '# '
+      '##'
+  steps(3)
+      '#  '
+      '## '
+      '###'
+  steps(4)
+      '#   '
+      '##  '
+      '### '
+      '####'
 
+// From 0 to n (iterate through rows)
+  // Create an empty string, 'stair'
+  // From 0 to n (iterate through columns)
+    // IF the current column is equal to or less than the current row
+      // Add a '#' to 'stair'
+    // ELSE
+      // Add a space to 'stair'
+  // Console log 'stair
+
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+
+    console.log(stair);
+  }
+}
