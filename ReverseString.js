2f9b405716c6def8fea3eadbcb3b26cccea73d76
Jesus Salomon <80434233+salomonj11@users.noreply.github.com>
2022-11-29 23:54:40 -0600

    Update ReverseString.js

--- ReverseString.js
+++ ReverseString.js
@@ -1,7 +1,7 @@
 --- Directions ---
 Given a string, return a new string with the reversed
 order of characters
---- Examples
+--- Examples ---
   reverse('apple') === 'leppa'
   reverse('hello') === 'olleh'
   reverse('Greetings!') === '!sgniteerG'
