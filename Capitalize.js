05ad1be36550141798a8b8a2f1fbdbb56cb39d36
Jesus Salomon <80434233+salomonj11@users.noreply.github.com>
2023-01-23 23:55:44 -0600

    Update Capitalize.js

--- Capitalize.js
+++ Capitalize.js
@@ -1,4 +1,4 @@
---- Directions
+--- Directions ---
 Write a function that accepts a string.  The function should
 capitalize the first letter of each word in the string then
 return the capitalized string.
